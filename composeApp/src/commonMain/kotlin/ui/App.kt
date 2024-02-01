2ff32db4a779d45fa7b61eb338f5c2a249a41f2c
Jayesh Patel <pateljayesh3737@gmail.com>
2024-02-01 15:45:26 +0530

    navigation flow

--- composeApp/src/commonMain/kotlin/ui/App.kt
+++ composeApp/src/commonMain/kotlin/ui/App.kt
@@ -2,12 +2,12 @@ package ui
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import ui.home.HomeScreen
+import ui.login.LoginScreen
 import ui.theme.ShareXpenseTheme
 
 @Composable
 fun App() {
     ShareXpenseTheme {
-        Navigator(screen = HomeScreen)
+        Navigator(screen = LoginScreen)
     }
 }
