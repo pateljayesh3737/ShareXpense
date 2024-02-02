043200175c68b3fb0fccb4b839a16c2a981b963e
Jayesh Patel <pateljayesh3737@gmail.com>
2024-02-03 01:12:53 +0530

    firebase login with email and password

--- composeApp/src/commonMain/kotlin/ui/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/login/LoginScreen.kt
@@ -6,15 +6,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -25,6 +36,7 @@ object LoginScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        val screenModel = rememberScreenModel { LoginScreenModel() }
 
         Scaffold { paddingValues ->
             Column(
@@ -33,26 +45,91 @@ object LoginScreen : Screen {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Text("Welcome to ShareXpense!", style = MaterialTheme.typography.displaySmall)
-                Spacer(Modifier.padding(vertical = 16.dp))
+                var email by remember { screenModel.email }
+                var password by remember { screenModel.password }
 
-                OutlinedTextField(value = "", onValueChange = {}, label = { Text("Username") })
-                Spacer(Modifier.padding(vertical = 10.dp))
+                val loginState by screenModel.state.collectAsState()
+                val errorMessageState by remember { screenModel.errorMessage }
 
-                OutlinedTextField(value = "", onValueChange = {}, label = { Text("Password") })
-                Spacer(Modifier.padding(vertical = 10.dp))
+                when (val state = loginState) {
+                    is LoginScreenModel.State.Error -> {
+                        println("Error found while logging in")
+                    }
 
-                ElevatedButton(onClick = { navigator.push(HomeScreen) }) {
-                    Text("Login", modifier = Modifier.padding(horizontal = 16.dp))
+                    is LoginScreenModel.State.Init -> {
+                        Text(
+                            "Welcome to ShareXpense!",
+                            style = MaterialTheme.typography.displaySmall
+                        )
+                        Spacer(Modifier.padding(vertical = 16.dp))
+
+                        OutlinedTextField(
+                            value = email,
+                            onValueChange = { email = it },
+                            label = { Text("Email") },
+                            singleLine = true,
+                            supportingText = {
+                                errorMessageState.first.let {
+                                    if (it.isNotEmpty()) Text(it)
+                                }
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Email,
+                                imeAction = ImeAction.Next
+                            ),
+                            isError = errorMessageState.first.isNotEmpty()
+                        )
+                        Spacer(Modifier.padding(vertical = 10.dp))
+
+                        OutlinedTextField(
+                            value = password,
+                            onValueChange = { password = it },
+                            label = { Text("Password") },
+                            visualTransformation = PasswordVisualTransformation(),
+                            singleLine = true,
+                            supportingText = {
+                                errorMessageState.second.let {
+                                    if (it.isNotEmpty()) Text(it)
+                                }
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Password,
+                                imeAction = ImeAction.Done
+                            ),
+                            isError = errorMessageState.second.isNotEmpty(),
+                        )
+                        Spacer(Modifier.padding(vertical = 10.dp))
+
+                        ElevatedButton(onClick = { screenModel.login() }) {
+                            Text("Login", modifier = Modifier.padding(horizontal = 16.dp))
+                        }
+
+                        Spacer(Modifier.padding(vertical = 10.dp))
+
+                        Text(
+                            "Don't have account? Sign up",
+                            Modifier.clickable { navigator.push(SignUpScreen) }.padding(all = 8.dp)
+                        )
+
+                    }
+
+                    is LoginScreenModel.State.Loading -> {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .size(50.dp)
+                                .padding(10.dp)
+                                .align(Alignment.CenterHorizontally)
+                        )
+                    }
+
+                    is LoginScreenModel.State.Success -> {
+                        val firebaseUser = state.firebaseUser
+                        println("Success : $firebaseUser")
+                        navigator.push(HomeScreen)
+                    }
                 }
-
-                Spacer(Modifier.padding(vertical = 10.dp))
-
-                Text("Don't have account? Sign up", Modifier.clickable { navigator.push(SignUpScreen) }.padding(all = 8.dp))
-
             }
         }
     }
-
 
 }
--- composeApp/src/commonMain/kotlin/ui/login/LoginScreenModel.kt
+++ composeApp/src/commonMain/kotlin/ui/login/LoginScreenModel.kt
@@ -1,7 +1,73 @@
 package ui.login
 
-import cafe.adriel.voyager.core.model.ScreenModel
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import cafe.adriel.voyager.core.model.StateScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.FirebaseAuth
+import dev.gitlive.firebase.auth.FirebaseAuthException
+import dev.gitlive.firebase.auth.FirebaseUser
+import dev.gitlive.firebase.auth.auth
+import kotlinx.coroutines.launch
 
-class LoginScreenModel : ScreenModel {
+class LoginScreenModel : StateScreenModel<LoginScreenModel.State>(State.Init) {
+    private val auth: FirebaseAuth = Firebase.auth
+    var email: MutableState<String> = mutableStateOf("jp87575@gmail.com")
+    var password: MutableState<String> = mutableStateOf("")
+    var errorMessage: MutableState<Pair<String, String>> = mutableStateOf(Pair("", ""))
 
+    init {
+        checkLoginState(auth.currentUser)
+    }
+
+    fun login() {
+        screenModelScope.launch {
+            mutableState.value = State.Loading
+
+            val email = email.value
+            val password = password.value
+
+            when {
+                email.isBlank() -> {
+                    mutableState.value = State.Init
+                    errorMessage.value = Pair("Invalid email address", "")
+                    return@launch
+                }
+
+                password.isBlank() -> {
+                    mutableState.value = State.Init
+                    errorMessage.value = Pair("", "Invalid password")
+                    return@launch
+                }
+
+                else -> {
+                    try {
+                        val authResult =
+                            auth.signInWithEmailAndPassword(email = email, password = password)
+
+                        checkLoginState(authResult.user)
+                    } catch (ex: FirebaseAuthException) {
+                        mutableState.value = State.Error(exception = ex)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLoginState(firebaseUser: FirebaseUser?) {
+        firebaseUser?.let {
+            mutableState.value = State.Success(firebaseUser = it)
+        } ?: run {
+            mutableState.value =
+                State.Error(exception = Throwable("Failed to login"))
+        }
+    }
+
+    sealed class State {
+        data object Init : State()
+        data object Loading : State()
+        data class Success(val firebaseUser: FirebaseUser) : State()
+        data class Error(val exception: Throwable) : State()
+    }
 }
